2d4aec93d25ef8079dcdcb8a49785319d673e4c1
Andrew Loeppky <Andrew.Loeppky@Gmail.com>
2020-06-13 16:16:12 -0700

    scraping works for .kml

--- scrape_hazmap.py
+++ scrape_hazmap.py
@@ -18,61 +18,49 @@ from os import system, name
 
 # %%
 # test run for a single case (august 1 2018)
+
 # url = "https://satepsanone.nesdis.noaa.gov/pub/FIRE/web/HMS/Smoke_Polygons/KML/2009/01/smoke20090101.kml"
 # filename = "C:/Users/Owner/Wildfire_Smoke_Mckendry/data/kml_smoke_polygons/smoke2018/smoke20090101.kml"
 
-url = "https://satepsanone.nesdis.noaa.gov/pub/FIRE/web/HMS/Smoke_Polygons/KML/2018/08/smoke20180801.kml"
-filename = "C:/Users/Owner/Wildfire_Smoke_Mckendry/data/kml_smoke_polygons/smoke2018/smoke20180801.kml"
+url = "https://satepsanone.nesdis.noaa.gov/pub/FIRE/web/HMS/Smoke_Polygons/KML/2009/07/smoke20090729.kml"
+filename = "C:/Users/Owner/Wildfire_Smoke_Mckendry/data/kml_smoke_polygons/smoke2009/smoke20090729.kml"
 
+# if the file exists, download and save
 r = requests.get(url)
-r = str(r)
-# get the file from url and save to to filename
-if r == "<Response [200]":
-    print("theres something here")
 
-elif r == "<Response [404]>":
-    print("theres nothing here")
+if str(r) == "<Response [200]>":
+    with open(filename, "wb") as code:
+        code.write(r.content)
+    # dwldcounder += 1
+    print("Downloading KML for date = [date]")
+    print("url = " + url)
+
+elif str(r) == "<Response [404]>":
+    print("No file found at date = [date]")
+    print("url = " + url)
 
 else:
-    print("something is wrong")
-
-#
-# with open(filename, "wb") as code:
-#    code.write(r.content)
-
-
-# %%
-# clear the screen between download messages
-def clear():
-    """
-    clear screen while executing in terminal
-    https://www.geeksforgeeks.org/clear-screen-python/
-    """
-    # for windows
-    if name == "nt":
-        _ = system("cls")
-
-    # for mac and linux(here, os.name is 'posix')
-    else:
-        _ = system("clear")
+    print("Crashed at date = [date]")
+    # break
 
 
 # %%
 # get all the .kml files and save them in a folder called smoke_kml, sorted by month
 base_url = "https://satepsanone.nesdis.noaa.gov/pub/FIRE/web/HMS/Smoke_Polygons/KML/"
 
-years = range(2009, 2021)
+years = range(2011, 2021)
 months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
 days = ["01", "02", "03", "04", "05", "06", "07", "08", "09"] + list(range(10, 32))
 
-counter = 0
+parsecounter = 0
+dwldcounder = 0
 # limit to a year of files for now
 for year in years:
     year = str(year)
     for month in months:
         month = str(month)
         for day in days:
-            if counter <= 365:
+            if parsecounter <= 365:
                 day = str(day)
 
                 # make variables that match website conventions
@@ -99,16 +87,31 @@ for year in years:
                     + ".kml"
                 )
 
-                # download and save
+                # if the file exists, download and save
                 r = requests.get(url)
-                with open(filename, "wb") as code:
-                    code.write(r.content)
+
+                if str(r) == "<Response [200]>":
+                    with open(filename, "wb") as code:
+                        code.write(r.content)
+                    dwldcounder += 1
+                    print(
+                        "Downloading KML for date = " + year + "/" + month + "/" + day
+                    )
+                    print("url = " + url)
+
+                elif str(r) == "<Response [404]>":
+                    print("No file found at date = " + year + "/" + month + "/" + day)
+                    print("url = " + url)
+
+                else:
+                    print("Crashed at date = " + year + "/" + month + "/" + day)
+                    break
 
                 # housekeeping
-                counter += 1
-                print("successfully downloaded " + str(counter) + " kml files")
+                parsecounter += 1
+                print(str(parsecounter) + " days parsed")
+                print(str(dwldcounder) + " files downloaded\n")
                 time.sleep(1)
-                clear()
 
 
 # %%
