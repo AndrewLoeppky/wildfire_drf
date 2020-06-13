6607515fa25c3f9b4d1613ea79adb85d2c911418
Andrew Loeppky <Andrew.Loeppky@Gmail.com>
2020-06-13 09:37:35 -0700

    clean up

--- scrape_hazmap.py
+++ scrape_hazmap.py
@@ -11,8 +11,34 @@
 #
 
 # %%
-from bs4 import BeautifulSoup
+import requests
+import urllib
+import time
 
+
+# %%
+url = "https://satepsanone.nesdis.noaa.gov/pub/FIRE/web/HMS/Smoke_Polygons/KML/2018/08/"
+filename = "smoke20180801.kml"  # or specify the whole path
+
+
+# downloading with requests
+
+# import the requests library
+import requests
+
+
+# download the file contents in binary format
+r = requests.get(url)
+
+# open method to open a file on your system and write the contents
+with open(filename, "wb") as code:
+    code.write(r.content)
+
+
+# %%
+
+
+# %%
 
 
 # %%
@@ -22,25 +48,12 @@ base_url = (
 )
 
 years = range(2005, 2021)
-months = [
-    "01",
-    "02",
-    "03",
-    "04",
-    "05",
-    "06",
-    "07",
-    "08",
-    "09",
-    "10",
-    "11",
-    "12",
-]
+months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
 
 for year in years:
-    year = str(year) + "/"
+    year = str(year)
     for month in months:
-        print(base_url + year + month + '/')
+        print(base_url + year + "/" + month + "/")
 
 
 # %%
