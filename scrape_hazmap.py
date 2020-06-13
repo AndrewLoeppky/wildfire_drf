2586ff0eed85ac9f6cc17d1ff9be55ce1bce1b8e
Andrew Loeppky <Andrew.Loeppky@Gmail.com>
2020-06-13 09:07:05 -0700

    add web scraping software

--- scrape_hazmap.py
+++ scrape_hazmap.py
@@ -11,6 +11,11 @@
 #
 
 # %%
+from bs4 import BeautifulSoup
+
+
+
+# %%
 # Loop through all urls with smoke shapefiles in them
 base_url = (
     "https://satepsanone.nesdis.noaa.gov/pub/FIRE/web/HMS/Smoke_Polygons/Shapefile/"
@@ -18,24 +23,24 @@ base_url = (
 
 years = range(2005, 2021)
 months = [
-    "01/",
-    "02/",
-    "03/",
-    "04/",
-    "05/",
-    "06/",
-    "07/",
-    "08/",
-    "09/",
-    "10/",
-    "11/",
-    "12/",
+    "01",
+    "02",
+    "03",
+    "04",
+    "05",
+    "06",
+    "07",
+    "08",
+    "09",
+    "10",
+    "11",
+    "12",
 ]
 
 for year in years:
     year = str(year) + "/"
     for month in months:
-        print(base_url + year + month)
+        print(base_url + year + month + '/')
 
 
 # %%
