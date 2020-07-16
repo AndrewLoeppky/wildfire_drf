71316fe024b57b1ac049db3efe3949f234be65a8
Andrew Loeppky <Andrew.Loeppky@Gmail.com>
2020-07-16 11:36:12 -0700

    update env

--- process_hazmap.py
+++ process_hazmap.py
@@ -1,7 +1,17 @@
+# =========================================================
+#                     Process Hazmap
+# =========================================================
+# uses geopandas package to loop through each day and
+# identify the level of smoke at the specified coordinates
+
 import geopandas as gpd
 import numpy as np
 from matplotlib import pyplot as plt
 
 
-print("its working")
+the_file = gpd.read_file(
+    "C:/Users/Owner/Wildfire_Smoke_Mckendry/data/shapefile_smoke_polygons/smoke2012/smoke20120703.shp"
+)
 
+the_plot = plt.plot(the_file)
+plt.show(the_plot)
