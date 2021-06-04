9ad2805c7ced1f752d06281af6cf79c8ab5a727e
Andrew Loeppky <Andrew.Loeppky@Gmail.com>
2021-06-04 11:19:31 -0600

    checkpoint process_hazmap

--- process_hazmap.py
+++ process_hazmap.py
@@ -24,10 +24,40 @@
 #
 # - save a plot of the site and smoke level as a png
 #
+#
+# **make sure coordinate systems match!**
 
-def 
+import fiona
+from shapely.geometry import Polygon, mapping 
+from matplotlib import pyplot as plt
+from descartes import PolygonPatch
 
+# field site coordinates
+site_lat, site_lon =  53.91439, 106.06958 # Waskisiu aerocan site
 
+# +
+# import a test shapefile. use august 1/18 when we know it was definitely smokey
+my_shp = "C:/Users/Owner/Wildfire_Smoke_Mckendry/data/shapefile_smoke_polygons/smoke2018/smoke20180801.shp"
+my_smoke = fiona.open(my_shp)
+
+# how to extract smoke density
+my_smoke[0]['properties']['Density']
+
+# +
+# loop through this whole process by date..
+# -
+
+# turn all separate shapes into Polygon objects
+polylist = []
+polyproperties = []
+for shape in my_smoke:
+    polygeom = Polygon(shape['geometry']['coordinates'][0])
+    polylist.append(polygeom)
+    polyproperties.append(shape['properties'])
+
+# +
+# create a small circle around field site coordinates
+# -
 
 
 
