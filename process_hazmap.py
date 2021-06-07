9fb7d1a7e620e5d32708975b6ecfcf6d0e0c1153
Andrew Loeppky <Andrew.Loeppky@Gmail.com>
2021-06-07 11:57:14 -0600

    make basic map with geopandas

--- process_hazmap.py
+++ process_hazmap.py
@@ -27,37 +27,77 @@
 #
 # **make sure coordinate systems match!**
 
-import fiona
-from shapely.geometry import Polygon, mapping 
 from matplotlib import pyplot as plt
-from descartes import PolygonPatch
-
-# field site coordinates
-site_lat, site_lon =  53.91439, 106.06958 # Waskisiu aerocan site
-
-# +
-# import a test shapefile. use august 1/18 when we know it was definitely smokey
-my_shp = "C:/Users/Owner/Wildfire_Smoke_Mckendry/data/shapefile_smoke_polygons/smoke2018/smoke20180801.shp"
-my_smoke = fiona.open(my_shp)
-
-# how to extract smoke density
-my_smoke[0]['properties']['Density']
+import geopandas as gpd
+from shapely.geometry import Point, Polygon
+# %matplotlib inline
 
 # +
-# loop through this whole process by date..
+## generate a geodataframe of field site(s)
+
+# lat/lon of field site (just wask for now)
+site_lat, site_lon =  53.91439, -106.06958
+
+# how big to display a plot
+disp_size = 10 # degrees
+sizeparam = disp_size / 2
+
+lat_point_list = [site_lat + sizeparam, site_lat + sizeparam, site_lat - sizeparam, site_lat - sizeparam]
+lon_point_list = [site_lon - sizeparam, site_lon + sizeparam, site_lon + sizeparam, site_lon - sizeparam]
+
+polygon_geom = Polygon(zip(lon_point_list, lat_point_list))
+crs = {'init': 'epsg:4326'}
+polygon = gpd.GeoDataFrame(index=[0], crs=crs, geometry=[polygon_geom])       
+print(polygon.geometry)
+
 # -
 
-# turn all separate shapes into Polygon objects
-polylist = []
-polyproperties = []
-for shape in my_smoke:
-    polygeom = Polygon(shape['geometry']['coordinates'][0])
-    polylist.append(polygeom)
-    polyproperties.append(shape['properties'])
+
+path_to_file = "C:/Users/Owner/Wildfire_Smoke_Mckendry/data/shapefile_smoke_polygons/smoke2018/smoke20180802.shp"
 
 # +
-# create a small circle around field site coordinates
+the_file = gpd.read_file(path_to_file)
+
+# get the light smoke
+light_smoke = the_file[the_file['Density']==5.0]
+
+# get the med smoke
+med_smoke = the_file[the_file['Density']==16.0]
+
+# get the heavy smoke
+heavy_smoke = the_file[the_file['Density']==27.0]
+
 # -
+
+# get a basemap
+world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+
+# +
+fig, ax = plt.subplots()
+
+ax.set_aspect("equal")
+world.plot(ax=ax, color='grey')
+polygon.plot(ax=ax, alpha=0.8)
+light_smoke.plot(ax=ax, color="yellow", alpha=0.5)
+med_smoke.plot(ax=ax, color="orange", alpha=0.5)
+heavy_smoke.plot(ax=ax, color="red", alpha=0.5)
+#site.plot(ax=ax)
+plt.show()
+# -
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
